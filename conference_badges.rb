b72eeaeba03af355e0871b08750a8bf69a6d0efa
David Thayer <dthayer16@gmail.com>
2019-03-01 21:18:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,15 +10,18 @@ def batch_badge_creator (names)
   array
 end
 
-def assign_rooms(names)
-  array = []
-  attendees.each_with_index.map do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
-def printer (speakers)
-  batch_badge_creator(speakers).each do |badge|
-  puts badge
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 end
